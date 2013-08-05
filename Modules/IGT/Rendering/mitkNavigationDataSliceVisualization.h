dc0b0f21ce3a599b98434b3ba2f0d463d18fdad4
Taylor Braun-Jones <taylor@braun-jones.org>
2013-08-05 14:40:20 -0400

    Add NavigationDataSliceVisualization filter to IGT module
    This filter allows slice position and orientation to be controlled by
    the position and orientation of navigation data.
    
    Signed-off-by: Taylor Braun-Jones <taylor@braun-jones.org>

--- Modules/IGT/Rendering/mitkNavigationDataSliceVisualization.h
+++ Modules/IGT/Rendering/mitkNavigationDataSliceVisualization.h
@@ -31,7 +31,7 @@ namespace mitk
 *
 * \ingroup IGT
 */
-class MITKIGT_EXPORT NavigationDataSliceVisualization : public NavigationDataToNavigationDataFilter
+class MitkIGT_EXPORT NavigationDataSliceVisualization : public NavigationDataToNavigationDataFilter
 {
   public:
     mitkClassMacro(NavigationDataSliceVisualization, NavigationDataToNavigationDataFilter)
