80520186f0a9d74b14b0292046349dc064ea309d
Aurélien Labrosse <aurelien@pollen-metrology.com>
2016-04-05 18:31:31 +0200

    Reconfigure patch for CTK (splashscreen)

--- CMakeExternals/CTK.cmake
+++ CMakeExternals/CTK.cmake
@@ -54,11 +54,9 @@ if(MITK_USE_CTK)
       list(APPEND proj_DEPENDENCIES DCMTK)
     else()
       list(APPEND ctk_optional_cache_args
-           -DDCMTK_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_DCMTK_85526e6.tar.gz
+           -DDCMTK_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_DCMTK_085525e6.tar.gz
           )
     endif()
-
-     set (ctk_qt_args -DCTK_QT_VERSION:STRING=${DESIRED_QT_VERSION})
 
     if(CTEST_USE_LAUNCHERS)
       list(APPEND ctk_optional_cache_args
@@ -72,87 +70,44 @@ if(MITK_USE_CTK)
       ENDIF()
     ENDFOREACH()
 
-	if(MITK_USE_Python)
-	  if(NOT DEFINED ENV{MITK_SOURCE})
-	    message(FATAL_ERROR "Environment variable MITK_SOURCE not set, set it and build again")
-	  endif()
-      ExternalProject_Add(${proj}
-        LIST_SEPARATOR ${sep}
-        URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_${revision_tag}.tar.gz
-        #GIT_REPOSITORY https://github.com/commontk/CTK.git
-        #GIT_TAG origin/master
-        URL_MD5 9ebeb78c78ff9f458045e0a5ecffc73f
-        PATCH_COMMAND ${PATCH_COMMAND} -N -p1 -i ${CMAKE_CURRENT_LIST_DIR}/CTK.patch
-        UPDATE_COMMAND ""
-        INSTALL_COMMAND ""
-        CMAKE_GENERATOR ${gen}
-        CMAKE_ARGS
-          ${ep_common_args}
-          ${ctk_optional_cache_args}
-          ${ctk_qt_args}
-          # The CTK PluginFramework cannot cope with
-          # a non-empty CMAKE_DEBUG_POSTFIX for the plugin
-          # libraries yet.
-          -DCMAKE_DEBUG_POSTFIX:STRING=
-          -DGit_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-          -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-          -DCTK_LIB_CommandLineModules/Backend/LocalProcess:BOOL=ON
-          -DCTK_LIB_CommandLineModules/Frontend/QtGui:BOOL=ON
-          -DCTK_LIB_PluginFramework:BOOL=ON
-          -DCTK_LIB_DICOM/Widgets:BOOL=ON
-          -DCTK_LIB_XNAT/Core:BOOL=ON
-          -DCTK_PLUGIN_org.commontk.eventadmin:BOOL=ON
-          -DCTK_PLUGIN_org.commontk.configadmin:BOOL=ON
-          -DCTK_USE_GIT_PROTOCOL:BOOL=OFF
-          -DDCMTK_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_DCMTK_085525e6.tar.gz
-          -DqRestAPI_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/qRestAPI_c5e4c2a7.tar.gz
-          # See bug 19073
-          -DPythonQt_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/PythonQt_36ab9c7c.tar.gz
-        CMAKE_CACHE_ARGS
-          ${ep_common_cache_args}
-        CMAKE_CACHE_DEFAULT_ARGS
-          ${ep_common_cache_default_args}
-        DEPENDS ${proj_DEPENDENCIES}
-       )
-	   else()
-		   ExternalProject_Add(${proj}
-			LIST_SEPARATOR ${sep}
-			URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_${revision_tag}.tar.gz
-			#GIT_REPOSITORY https://github.com/commontk/CTK.git
-			#GIT_TAG origin/master
-			URL_MD5 9ebeb78c78ff9f458045e0a5ecffc73f
-			UPDATE_COMMAND ""
-			INSTALL_COMMAND ""
-			CMAKE_GENERATOR ${gen}
-			CMAKE_ARGS
-			  ${ep_common_args}
-			  ${ctk_optional_cache_args}
-			  ${ctk_qt_args}
-			  # The CTK PluginFramework cannot cope with
-			  # a non-empty CMAKE_DEBUG_POSTFIX for the plugin
-			  # libraries yet.
-			  -DCMAKE_DEBUG_POSTFIX:STRING=
-			  -DGit_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-			  -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-			  -DCTK_LIB_CommandLineModules/Backend/LocalProcess:BOOL=ON
-			  -DCTK_LIB_CommandLineModules/Frontend/QtGui:BOOL=ON
-			  -DCTK_LIB_PluginFramework:BOOL=ON
-			  -DCTK_LIB_DICOM/Widgets:BOOL=ON
-			  -DCTK_LIB_XNAT/Core:BOOL=ON
-			  -DCTK_PLUGIN_org.commontk.eventadmin:BOOL=ON
-			  -DCTK_PLUGIN_org.commontk.configadmin:BOOL=ON
-			  -DCTK_USE_GIT_PROTOCOL:BOOL=OFF
-			  -DDCMTK_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_DCMTK_085525e6.tar.gz
-			  -DqRestAPI_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/qRestAPI_c5e4c2a7.tar.gz
-			  # See bug 19073
-			  -DPythonQt_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/PythonQt_36ab9c7c.tar.gz
-			CMAKE_CACHE_ARGS
-			  ${ep_common_cache_args}
-			CMAKE_CACHE_DEFAULT_ARGS
-			  ${ep_common_cache_default_args}
-			DEPENDS ${proj_DEPENDENCIES}
-		   )
-	   endif()
+    ExternalProject_Add(${proj}
+      LIST_SEPARATOR ${sep}
+      URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_${revision_tag}.tar.gz
+      #GIT_REPOSITORY https://github.com/commontk/CTK.git
+      #GIT_TAG origin/master
+      URL_MD5 9ebeb78c78ff9f458045e0a5ecffc73f
+      UPDATE_COMMAND ""
+	  PATCH_COMMAND ${PATCH_COMMAND} -N -p1 -i ${CMAKE_CURRENT_LIST_DIR}/ctk-splashscreen.patch
+      INSTALL_COMMAND ""
+      CMAKE_GENERATOR ${gen}
+      CMAKE_ARGS
+        ${ep_common_args}
+        ${ctk_optional_cache_args}
+        # The CTK PluginFramework cannot cope with
+        # a non-empty CMAKE_DEBUG_POSTFIX for the plugin
+        # libraries yet.
+        -DCMAKE_DEBUG_POSTFIX:STRING=
+        -DCTK_QT_VERSION:STRING=5
+        -DGit_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+        -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+        -DCTK_LIB_CommandLineModules/Backend/LocalProcess:BOOL=ON
+        -DCTK_LIB_CommandLineModules/Frontend/QtGui:BOOL=ON
+        -DCTK_LIB_PluginFramework:BOOL=ON
+        -DCTK_LIB_DICOM/Widgets:BOOL=ON
+        -DCTK_LIB_XNAT/Core:BOOL=ON
+        -DCTK_PLUGIN_org.commontk.eventadmin:BOOL=ON
+        -DCTK_PLUGIN_org.commontk.configadmin:BOOL=ON
+        -DCTK_USE_GIT_PROTOCOL:BOOL=OFF
+        -DDCMTK_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_DCMTK_085525e6.tar.gz
+        -DqRestAPI_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/qRestAPI_c5e4c2a7.tar.gz
+        # See bug 19073
+        -DPythonQt_URL:STRING=${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/PythonQt_36ab9c7c.tar.gz
+      CMAKE_CACHE_ARGS
+        ${ep_common_cache_args}
+      CMAKE_CACHE_DEFAULT_ARGS
+        ${ep_common_cache_default_args}
+      DEPENDS ${proj_DEPENDENCIES}
+     )
 
     ExternalProject_Get_Property(${proj} binary_dir)
     set(CTK_DIR ${binary_dir})
