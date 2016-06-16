7c87b4c68372d1a4cea8c241d3e6cbbad9d45c33
BERNARD GUILHEM <GBE13158@teamlog.intra>
2016-06-16 10:57:25 +0200

    Added localSwitch.h reference

--- Modules/SceneSerializationBase/src/mitkTransferFunctionPropertySerializer.cpp
+++ Modules/SceneSerializationBase/src/mitkTransferFunctionPropertySerializer.cpp
@@ -15,6 +15,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 ===================================================================*/
 
 #include "mitkTransferFunctionPropertySerializer.h"
+#include <mitkLocaleSwitch.h>
 #include <boost/lexical_cast.hpp>
 
 namespace mitk {
