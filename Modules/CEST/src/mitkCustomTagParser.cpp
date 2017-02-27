5e6d711e51a71ec3c39de3b8aa0102c6b4612ad0
Caspar Goch <c.goch@dkfz-heidelberg.de>
2017-02-27 17:58:49 +0100

    Linux compatibility fixes

--- Modules/CEST/src/mitkCustomTagParser.cpp
+++ Modules/CEST/src/mitkCustomTagParser.cpp
@@ -319,7 +319,7 @@ std::vector<int> mitk::CustomTagParser::GetExternalRevisions()
   return availableRevisionsVector;
 }
 
-std::string mitk::CustomTagParser::GetClosestLowerRevision(std::string revisionString, std::vector<int> &availableRevisionsVector)
+std::string mitk::CustomTagParser::GetClosestLowerRevision(std::string revisionString, std::vector<int> availableRevisionsVector)
 {
 
   // descending order
@@ -508,7 +508,7 @@ std::string mitk::CustomTagParser::GetOffsetString(std::string samplingType, std
 
       std::sort(offsetVector.begin(), offsetVector.end(), std::greater<>());
 
-      for (int index = 0; index < offsetVector.size(); ++index)
+      for (unsigned int index = 0; index < offsetVector.size(); ++index)
       {
         offsetVector[index] = std::pow(-1, index) * offsetVector[index];
       }
