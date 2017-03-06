cfafcaf338f7bf8e67709d81b2ecf51e35c3cb91
Aurélien Labrosse <aurelien@pollen-metrology.com>
2017-03-06 17:38:40 +0100

    T22591 fix for SimpleITK/system Python/Windows
    * Add a condition to have a Windows-compatible expression
      for SimpleITK egg installation
    
    * The PYTHONPATH variable has to be set BEFORE CMake is called
      if system Python is used

--- CMakeExternals/SimpleITK.cmake
+++ CMakeExternals/SimpleITK.cmake
@@ -110,11 +110,19 @@ if(MITK_USE_SimpleITK)
       endif()
 
       if( MITK_USE_SYSTEM_PYTHON )
-        ExternalProject_Add_Step(${proj} sitk_python_install_step
-          COMMAND PYTHONUSERBASE=${_install_dir} ${PYTHON_EXECUTABLE} setup.py install --prefix=${_install_dir}
-          DEPENDEES install
-          WORKING_DIRECTORY ${_sitk_build_dir}/Wrapping/PythonPackage
-        )
+        if(WIN32)
+           # if environment variable PYTHONUSERBASE does not
+           # contain ${_install_dir}, installation here will fail.
+           ExternalProject_Add_Step(${proj} sitk_python_install_step
+           COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix=${_install_dir}
+            DEPENDEES install
+            WORKING_DIRECTORY ${_sitk_build_dir}/Wrapping/PythonPackage)
+        else()
+           ExternalProject_Add_Step(${proj} sitk_python_install_step
+           COMMAND PYTHONUSERBASE=${_install_dir} ${PYTHON_EXECUTABLE} setup.py install --prefix=${_install_dir}
+           DEPENDEES install
+           WORKING_DIRECTORY ${_sitk_build_dir}/Wrapping/PythonPackage)
+         endif()
       else()
         ExternalProject_Add_Step(${proj} sitk_python_install_step
           COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix=${_install_dir}
